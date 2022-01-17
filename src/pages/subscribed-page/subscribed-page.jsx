8e342eaf90835771a149a90e637f3eef7abf3a46
Your Name <you@example.com>
2022-01-17 07:09:45 +0200

    Handling subs Unauthorized exception

--- src/pages/subscribed-page/subscribed-page.jsx
+++ src/pages/subscribed-page/subscribed-page.jsx
@@ -1,29 +1,34 @@
 import React, { useEffect } from 'react';
 import { useHistory, useParams } from 'react-router-dom';
+import { useDispatch } from 'react-redux';
 
 import { useGetSubscribedQuery } from 'api/user-api';
 import Loader from 'components-ui/loader/loader';
 import UserList from 'components/user-list/user-list';
+import { showErrorNotification } from 'store/notification-slice';
 
 const SubscribedPage = () => {
   const authorId = useParams().id;
   const history = useHistory();
-  const { data, isFetching, isSuccess, isError } = useGetSubscribedQuery(authorId) ;
+  const { data, isFetching, isSuccess, isError, error } = useGetSubscribedQuery(authorId) ;
+  const dispatch = useDispatch();
 
   useEffect(() => {
-    if (isError) history.replace('/');
-  }, [isError, history]);
+    if (isError) {
+      dispatch(showErrorNotification(error.data.message));
+      history.replace('/');
+    }
+
+  }, [isError, history, dispatch, error?.data?.message]);
 
   if (isFetching)
     return <Loader />;
 
-
-  if (isSuccess)
-    return (
-      <div>
-        <UserList users={data} title="User subscribed" />
-      </div>
-    );
+  return (
+    <div>
+      {(isSuccess && data) && <UserList users={data} title="User subscribed" />}
+    </div>
+  );
 };
 
 export default SubscribedPage;
--- src/pages/subscribers-page/subscribers-page.jsx
+++ src/pages/subscribers-page/subscribers-page.jsx
@@ -1,28 +1,35 @@
 import React, { useEffect } from 'react';
 import { useHistory, useParams } from 'react-router-dom';
+import { useDispatch } from 'react-redux';
 
 import { useGetSubscribersQuery } from 'api/user-api';
 import Loader from 'components-ui/loader/loader';
 import UserList from 'components/user-list/user-list';
+import { showErrorNotification } from 'store/notification-slice';
 
 const SubscribersPage = () => {
   const authorId = useParams().id;
   const history = useHistory();
-  const { data, isFetching, isSuccess, isError } = useGetSubscribersQuery(authorId) ;
+  const { data, isFetching, isSuccess, isError, error } = useGetSubscribersQuery(authorId) ;
+
+  const dispatch = useDispatch();
 
   useEffect(() => {
-    if (isError) history.replace('/');
-  }, [isError, history]);
+    if (isError) {
+      dispatch(showErrorNotification(error.data.message));
+      history.replace('/');
+    }
+
+  }, [isError, history, dispatch, error?.data?.message]);
 
   if (isFetching)
     return <Loader />;
 
-  if (isSuccess)
-    return (
-      <div>
-        <UserList users={data} title="User subscribers" />
-      </div>
-    );
+  return (
+    <div>
+      {isSuccess && <UserList users={data} title="User subscribers" />}
+    </div>
+  );
 };
 
 export default SubscribersPage;
